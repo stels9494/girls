1c6acc1b241665f4f09c8626735af4d32c7b9805
Ivanov Andrey <stels9494@mail.ru>
2020-05-26 18:07:41 +0000

    fix url fonts

---
+++ resources/views/order.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 text-md-left">
+			Reservation Form
+		</div>
+		<div class="col-md-6 text-md-right">
+			Enter promotional code
+			<input type="text" placeholder="Promo code">
+		</div>
+		<div class="col-4">
+			<label for="">Your name (nick)</label>
+			<input type="text" placeholder="name" required>
+		</div>
+		<div class="col-4">
+		</div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+		<div class="col-4"></div>
+	</div>
+</div>
